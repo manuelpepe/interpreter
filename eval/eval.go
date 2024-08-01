bba12266307f09312a4023a3317976483f2a88ef
Manuel Pepe <manuelpepe-dev@outlook.com.ar>
2024-08-01 17:24:44 -0300

    feat: eval infix expressions

--- eval/eval.go
+++ eval/eval.go
@@ -27,8 +27,51 @@ func Eval(node ast.Node) object.Object {
 	case *ast.PrefixExpression:
 		right := Eval(node.Right)
 		return evalPrefixExpression(node.Operator, right)
+	case *ast.InfixExpression:
+		left := Eval(node.Left)
+		right := Eval(node.Right)
+		return evalInfixExpression(node.Operator, left, right)
 	}
 	return nil
+}
+
+func evalInfixExpression(operator string, left object.Object, right object.Object) object.Object {
+	switch {
+	case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
+		return evalIntegerInfixExpression(operator, left, right)
+	case operator == "==":
+		return evalBoolean(left == right)
+	case operator == "!=":
+		return evalBoolean(left != right)
+	default:
+		return NULL // TODO: Should probably error
+	}
+}
+
+func evalIntegerInfixExpression(operator string, left object.Object, right object.Object) object.Object {
+	leftVal := left.(*object.Integer).Value
+	rightVal := right.(*object.Integer).Value
+
+	switch operator {
+	case "-":
+		return &object.Integer{Value: leftVal - rightVal}
+	case "+":
+		return &object.Integer{Value: leftVal + rightVal}
+	case "/":
+		return &object.Integer{Value: leftVal / rightVal}
+	case "*":
+		return &object.Integer{Value: leftVal * rightVal}
+	case "<":
+		return evalBoolean(leftVal < rightVal)
+	case ">":
+		return evalBoolean(leftVal > rightVal)
+	case "==":
+		return evalBoolean(leftVal == rightVal)
+	case "!=":
+		return evalBoolean(leftVal != rightVal)
+	default:
+		return NULL
+	}
 }
 
 func evalPrefixExpression(operator string, right object.Object) object.Object {
