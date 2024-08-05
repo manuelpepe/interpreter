01338b31c2b1d7ab2d52427977897e2a1d15c0cd
Manuel Pepe <manuelpepe-dev@outlook.com.ar>
2024-08-05 02:09:00 -0300

    feat: builtins

--- eval/builtins.go
+++ eval/builtins.go
@@ -1,9 +1,18 @@
 package eval
 
-import "github.com/manuelpepe/interpreter/object"
+import (
+	"fmt"
+
+	"github.com/manuelpepe/interpreter/object"
+)
 
 var builtins = map[string]*object.Builtin{
-	"len": {Fn: &LenBuiltin{}},
+	"len":     {Fn: &LenBuiltin{}},
+	"first":   {Fn: &FirstBuiltin{}},
+	"last":    {Fn: &LastBuiltin{}},
+	"rest":    {Fn: &RestBuiltin{}},
+	"push":    {Fn: &PushBuiltin{}},
+	"inspect": {Fn: &InspectBuiltin{}},
 }
 
 func checkArgs(n int, args []object.Object) (bool, *object.Error) {
@@ -31,4 +40,104 @@ func (lb *LenBuiltin) Do(args ...object.Object) object.Object {
 }
 func (lb *LenBuiltin) Name() string {
 	return "len"
+}
+
+type FirstBuiltin struct{}
+
+func (fb *FirstBuiltin) Do(args ...object.Object) object.Object {
+	if ok, err := checkArgs(1, args); !ok {
+		return err
+	}
+	if args[0].Type() != object.ARRAY_OBJ {
+		return newError("argument to `first` not suported, got %s", args[0].Type())
+	}
+
+	arg := args[0].(*object.Array)
+	if len(arg.Items) > 0 {
+		return arg.Items[0]
+	}
+
+	return NULL
+}
+func (fb *FirstBuiltin) Name() string {
+	return "first"
+}
+
+type LastBuiltin struct{}
+
+func (fb *LastBuiltin) Do(args ...object.Object) object.Object {
+	if ok, err := checkArgs(1, args); !ok {
+		return err
+	}
+	if args[0].Type() != object.ARRAY_OBJ {
+		return newError("argument to `last` not suported, got %s", args[0].Type())
+	}
+
+	arg := args[0].(*object.Array)
+	if countItems := len(arg.Items); countItems > 0 {
+		return arg.Items[countItems-1]
+	}
+
+	return NULL
+}
+func (fb *LastBuiltin) Name() string {
+	return "last"
+}
+
+type RestBuiltin struct{}
+
+func (rb *RestBuiltin) Do(args ...object.Object) object.Object {
+	if ok, err := checkArgs(1, args); !ok {
+		return err
+	}
+	if args[0].Type() != object.ARRAY_OBJ {
+		return newError("argument to `rest` not suported, got %s", args[0].Type())
+	}
+
+	arr := args[0].(*object.Array)
+	if length := len(arr.Items); length > 0 {
+		newItems := make([]object.Object, length-1)
+		copy(newItems, arr.Items[1:length])
+		return &object.Array{Items: newItems}
+	}
+
+	return NULL
+}
+func (rb *RestBuiltin) Name() string {
+	return "rest"
+}
+
+type PushBuiltin struct{}
+
+func (pb *PushBuiltin) Do(args ...object.Object) object.Object {
+	if ok, err := checkArgs(2, args); !ok {
+		return err
+	}
+	if args[0].Type() != object.ARRAY_OBJ {
+		return newError("argument to `push` not suported, got %s", args[0].Type())
+	}
+
+	arr := args[0].(*object.Array)
+	length := len(arr.Items)
+	newItems := make([]object.Object, length+1)
+	copy(newItems, arr.Items)
+	newItems[length] = args[1]
+
+	return &object.Array{Items: newItems}
+}
+func (pb *PushBuiltin) Name() string {
+	return "push"
+}
+
+type InspectBuiltin struct{}
+
+func (ib *InspectBuiltin) Do(args ...object.Object) object.Object {
+	for _, arg := range args {
+		fmt.Printf("%s\n", arg.Inspect())
+	}
+	return NULL
+}
+
+func (ib *InspectBuiltin) Name() string {
+	return "inspect"
 }
--- main.go
+++ main.go
@@ -2,14 +2,19 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"os/user"
 
+	"github.com/manuelpepe/interpreter/eval"
 	"github.com/manuelpepe/interpreter/graph"
+	"github.com/manuelpepe/interpreter/lexer"
+	"github.com/manuelpepe/interpreter/object"
+	"github.com/manuelpepe/interpreter/parser"
 	"github.com/manuelpepe/interpreter/repl"
 )
 
-const option = 1
+const option = 3
 
 func main() {
 	switch option {
@@ -17,8 +22,33 @@ func main() {
 		doREPL()
 	case 2:
 		doGraph()
+	case 3:
+		doRunFile(os.Args[1])
 	default:
 		panic("unknown option")
+	}
+}
+
+func doRunFile(srcFile string) {
+	data, err := os.ReadFile(srcFile)
+	if err != nil {
+		fmt.Fprintf(os.Stdout, "Error opening file: %v\n", err)
+		return
+	}
+
+	l := lexer.NewLexer(string(data))
+	p := parser.New(l)
+	prog := p.ParseProgram()
+	if len(p.Errors()) != 0 {
+		repl.PrintParserErrors(os.Stdout, p.Errors())
+		return
+	}
+
+	env := object.NewEnvironment()
+	res := eval.Eval(prog, env)
+	if res != nil {
+		io.WriteString(os.Stdout, res.Inspect())
+		io.WriteString(os.Stdout, "\n")
 	}
 }
 
